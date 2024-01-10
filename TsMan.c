3235c656d0ed3a06dd5e140b8b70929782e176f5
100dollarguy <github@100dollarguy.co.in>
2024-01-10 19:34:49 +0530

    Second Program

---
+++ TsMan.c
@@ -0,0 +1,8 @@
+//
+//  TsMan.c
+//  DAA programs
+//
+//  Created by ROHAN on 10/01/24.
+//
+
+#include "TsMan.h"
---
+++ TsMan.h
@@ -0,0 +1,13 @@
+//
+//  TsMan.h
+//  DAA programs
+//
+//  Created by ROHAN on 10/01/24.
+//
+
+#ifndef TsMan_h
+#define TsMan_h
+
+#include <stdio.h>
+
+#endif /* TsMan_h */
