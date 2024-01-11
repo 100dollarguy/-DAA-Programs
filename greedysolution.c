cfeb13a4d68cc8f00876c1fb347a88799ef0b7e7
100dollarguy <github@100dollarguy.co.in>
2024-01-11 09:00:23 +0530

    Fourth  program

---
+++ greedysolution.c
@@ -0,0 +1,8 @@
+//
+//  greedysolution.c
+//  DAA programs
+//
+//  Created by ROHAN on 11/01/24.
+//
+
+#include "greedysolution.h"
---
+++ greedysolution.h
@@ -0,0 +1,13 @@
+//
+//  greedysolution.h
+//  DAA programs
+//
+//  Created by ROHAN on 11/01/24.
+//
+
+#ifndef greedysolution_h
+#define greedysolution_h
+
+#include <stdio.h>
+
+#endif /* greedysolution_h */
