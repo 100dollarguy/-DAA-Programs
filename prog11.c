e931672f3bed413d8f9be3b37a3f0cc72cd4c96d
𝓛𝓪 𝓢𝓪𝓵𝓿𝓪𝓽𝓸𝓻𝓮 -1864 <bigbadwolf@tutamail.com>
2024-01-31 11:41:19 +0530

    write a c program that accepts the vertices and edges for a graph and stores it as an adjacency matrix

---
+++ prog11.c
@@ -0,0 +1,57 @@
+//
+//  prog11.c
+//  DAA programs
+//
+//  Created by ROHAN on 31/01/24.
+//
+
+#include "prog11.h"
+#include <stdio.h>
+
+void printAdjacencyMatrix(int maxVertices, int adjacencyMatrix[][maxVertices]) {
+    int i, j;
+    printf("Adjacency Matrix of the Graph:\n");
+    for (i = 0; i < maxVertices; i++) {
+        for (j = 0; j < maxVertices; j++) {
+            printf("%d ", adjacencyMatrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int maxVertices, numEdges, i, j;
+
+    printf("Enter the number of vertices:");
+    scanf("%d", &maxVertices);
+
+    int adjacencyMatrix[maxVertices][maxVertices];
+
+    for (i = 0; i < maxVertices; i++) {
+        for (j = 0; j < maxVertices; j++) {
+            adjacencyMatrix[i][j] = 0;
+        }
+    }
+
+    printf("Enter the number of edges:");
+    scanf("%d", &numEdges);
+
+    printf("Enter the edges (vertex pairs, e.g., 1,2):\n");
+
+    for (i = 0; i < numEdges; i++) {
+        int vertex1, vertex2;
+        scanf("%d%d", &vertex1, &vertex2);
+
+        if (vertex1 >= 1 && vertex1 <= maxVertices && vertex2 >= 1 && vertex2 <= maxVertices) {
+            adjacencyMatrix[vertex1 - 1][vertex2 - 1] = 1;
+            adjacencyMatrix[vertex2 - 1][vertex1 - 1] = 1; // Corrected index
+        } else {
+            printf("Invalid edge. Vertex numbers should be between 1 and %d.\n", maxVertices);
+            i--;
+        }
+    }
+
+    printAdjacencyMatrix(maxVertices, adjacencyMatrix);
+
+    return 0;
+}
---
+++ prog11.h
@@ -0,0 +1,13 @@
+//
+//  prog11.h
+//  DAA programs
+//
+//  Created by ROHAN on 31/01/24.
+//
+
+#ifndef prog11_h
+#define prog11_h
+
+#include <stdio.h>
+
+#endif /* prog11_h */
